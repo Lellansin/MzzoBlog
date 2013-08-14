04e1c6f3b058e40e314907c15f26947dada886c2
stone <346135265@qq.com>
2013-08-14 13:12:03 +0800

    huangxh

---
+++ src/kernel/lib/SqlTool.class.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * 数据库操作类
+ *
+ * @author huangxh
+ * @version 1.0
+ */
+
+
+ 	class SqlTool{
+
+ 		private $conn;
+ 		private $dbname="empmanage";
+ 		private $host="localhost";
+ 		private $user="root";
+ 		private $password="";
+
+ 		function SqlTool()
+ 		{
+ 			$this->conn=mysql_connect($this->host,$this->user,$this->password);
+ 			if (!$this->conn) {
+ 				die("数据库连接失败".mysql_error());
+ 			} 			
+ 			mysql_select_db($this->dbname,$this->conn);
+ 			mysql_query("set names utf8")or die(mysql_error());
+ 		}
+
+/**
+ * 	数据库查询
+ *	语句 select
+ * 	
+ */
+
+ 		public function dql($sql){
+ 			$array=array();
+ 			$result=mysql_query($sql,$this->conn)or die(mysql_error());
+ 		}
+
+
+ 	}
+
+
+?>
