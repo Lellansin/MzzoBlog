11e7c549c6292db5393e5111f4a554beadf31052
stone <346135265@qq.com>
2013-08-24 09:40:32 +0800

    框架学习
    第二步编写

--- src/controller/article/article.php
+++ src/controller/article/article.php
@@ -1,5 +1,15 @@
-<?
 
+<?php
+header("Content-Type: text/html; charset=utf8");
+$sql="select * from mz_posts where id=$id";
+$sqltool=new SqlTool();
+$result=$sqltool->dql($sql);
+if (!$result) {
+	echo "没有找到该文章";
+	exit();
+}
+
+require "./view/article/article.html";
 /*
 
 通过路径找到要查询的文章id
@@ -7,3 +17,4 @@
 调用数据库操作类查询相应id的文章
 
 */
+?>
--- src/kernel/mzzo.php
+++ src/kernel/mzzo.php
@@ -1,5 +1,5 @@
 <?php
-
+require "./kernel/lib/SqlTool.class.php";
 /*
 
 在这里调用 /kernel/lib/SqlTool.class.php 使得之后的控制器都能够调用数据库操作类
@@ -7,7 +7,12 @@
 */
 
 $path=$_GET["path"];
-$path="./controller/".substr($path,0,-4)."php";
+	$ids=(strpos($path,'_')+1);
+	$ide=(strpos($path,'.')-$ids);
+	$id=substr($path,$ids,$ide);
+
+
+$path="./controller/".substr_replace($path,'.',$ids-1)."php";
 require "$path";
 
 
